e19a005dd72b77d80f9d0cbf49e81adf15c52344
Paul <paulb896@gmail.com>
2014-03-02 23:11:48 -0800

    Change banner style

--- public/js/controllers/date-picker-controller.js
+++ public/js/controllers/date-picker-controller.js
@@ -81,7 +81,6 @@ angular.module('reserveTheTime.controllers.datePicker', [])
 
         console.log("date initialized", $scope.UserSelection);
         Draggable.create("#day-picker", {type:"y", edgeResistance:0.3, throwProps:true,bounds:{heigjt:600, bottom:100, left:0, top:-110}});
-        TweenListe.to("#day-picker", {z:0.1});
     };
 
     $scope.updateReservations = function(){
