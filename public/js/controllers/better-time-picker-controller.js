91bb53b7f0e2fd48142d41934371e56c34361a20
Paul <paulb896@gmail.com>
2014-03-02 23:26:44 -0800

    Change banner style

--- public/js/controllers/better-time-picker-controller.js
+++ public/js/controllers/better-time-picker-controller.js
@@ -28,7 +28,7 @@ angular.module('reserveTheTime.controllers.betterTimePicker', [])
             $scope.setHourPicker();
         }
 
-        Draggable.create("#day-picker", {type:"x", edgeResistance:0.3, throwProps:true,bounds:{height:600, bottom:0, left:0, maxX:250, minX:-250}});
+        //Draggable.create("#day-picker", {type:"x", edgeResistance:0.3, throwProps:true,bounds:{height:600, bottom:0, left:0, maxX:250, minX:-250}});
     };
 
     $scope.clearTimeCircle = function() {
